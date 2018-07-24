9a26f1516c409a66a865e7d5c7adee52e3613125
Neale Pickett <neale@lanl.gov>
2018-07-24 23:39:13 +0000

    Document the whole package

---
+++ doc.go
@@ -0,0 +1,9 @@
+/*
+
+Package netshovel provides utilities to assist in creating of application-layer protocol decoders.
+
+examples/simple/simple.go contains a full decoder which does nothing but dump every utterance.
+It can be used as a template for new work.
+
+*/
+package netshovel
